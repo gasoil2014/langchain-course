6db4b7a708702350ba60cc3676916c23cbb73d6f
Gaston Mourin <gaston.mourin@primetec.com.ar>
2025-10-08 14:43:33 -0300

    with_structured_output

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from langchain.agents.react.agent import create_react_agent
 from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
 from langchain_tavily import TavilySearch
-from langchain_core.output_parsers.pydantic import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableLambda
 
@@ -16,31 +15,30 @@ from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
 from schemas import AgentResponse
 
 tools = [TavilySearch()]
-llm = ChatOpenAI(model="gpt-4o")
-#llm = ChatOllama(model="gpt-oss:20b",temperature=0)
-react_prompt = hub.pull("hwchase17/react")
+llm = ChatOpenAI(model="gpt-4o-mini")
+structured_llm = llm.with_structured_output(AgentResponse)
 
-output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+#llm = ChatOllama(model="gpt-oss:20b",temperature=0)
+#react_prompt = hub.pull("hwchase17/react")
+
 react_prompt_with_format_instructions = PromptTemplate(
     template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
     input_variables=["input", "agent_scratchpad", "tool_names","tools"],
-).partial(format_instructions=output_parser.get_format_instructions())
+).partial(format_instructions="")
 
 
 agent = create_react_agent(llm, tools, prompt=react_prompt_with_format_instructions)
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
 
 extract_output = RunnableLambda(lambda x: x["output"])
 
-parse_output = RunnableLambda(lambda x: output_parser.parse(x))
-
-chain = agent_executor | extract_output | parse_output
+chain = agent_executor | extract_output | structured_llm
 
 def main():
     print("Hello from langchain-course!")
     result = chain.invoke(
         {
-            "input": "search in linkedin 3 job postings for an ai engineer in new york and list their results and links",
+            "input": "decime los principales servicios de la empresa primetec.com.ar",
         }
     )
     print(result)
